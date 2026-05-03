53f03767c30c46e352738886cbe1994d46cfd31e
cfarro <cfarro@eada.net>
2026-05-03 18:27:55 +0200

    enum code

---
+++ trading_engine/enums.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Side(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+class OrderType(Enum):
+    MARKET = "MARKET"
+    LIMIT = "LIMIT"
+    STOP_LIMIT = "STOP_LIMIT"
+
+class OrderStatus(Enum):
+    NEW = "NEW"
+    FILLED = "FILLED"
+    PARTIALLY_FILLED = "PARTIAL"
+    CANCELLED = "CANCELLED"
+    TRIGGERED = "TRIGGERED"
+    
