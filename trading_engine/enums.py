e4b9c4ba14e1382e1cf4e3a2878c2a9bb8a136c7
Chloë Farro <cfarro@eada.net>
2026-09-24 18:30:51 +0200

    Refactor enums to inherit from str and extend functionality
    Updated enum classes to inherit from str and added new order types and statuses.

--- trading_engine/enums.py
+++ trading_engine/enums.py
@@ -1,18 +1,29 @@
 from enum import Enum
 
-class Side(Enum):
+class Side(str, Enum):
     BUY = "BUY"
     SELL = "SELL"
 
-class OrderType(Enum):
+class OrderType(str, Enum):
     MARKET = "MARKET"
     LIMIT = "LIMIT"
     STOP_LIMIT = "STOP_LIMIT"
+    OCO = "OCO"
+    OTO = "OTO"
 
-class OrderStatus(Enum):
-    NEW = "NEW"
+class OrderStatus(str, Enum):
+    CREATED = "CREATED"
+    SUBMITTED = "SUBMITED"
+    OPEN = "OPEN"
     FILLED = "FILLED"
     PARTIALLY_FILLED = "PARTIAL"
     CANCELLED = "CANCELLED"
-    TRIGGERED = "TRIGGERED"
+    REJECTED = "REJECTED"
+    EXPIRED = "EXPIRED"
+    PENDING = "PENDING"
+
+class TimeInForce(str, Enum):
+    GTC = "GTC" 
+    IOC = "IOC"
+    FOK = "FOK"
     
